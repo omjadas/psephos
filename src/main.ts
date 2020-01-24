f8399c2d2de932353cdae402eea3fb49c87872f8
omjadas <omja.das@gmail.com>
2020-01-24 22:46:25 +1100

    set up scaffolding

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+const port = process.env.PORT || 3000;
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+
+bootstrap();
