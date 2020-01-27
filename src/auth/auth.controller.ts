6530e64e131b4fe8340510a238c177ca3c0e574e
omjadas <omja.das@gmail.com>
2020-01-27 11:46:51 +1100

    set jwt as cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Req, UseGuards, Redirect, HttpStatus } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { CookieSettings, SetCookies } from "@nestjsplus/cookies";
 import { Request } from "express";
 import { AuthService } from "./auth.service";
+
+interface MyRequest extends Request {
+  _cookies: CookieSettings[],
+}
 
 @Controller("auth")
 export class AuthController {
@@ -13,15 +18,37 @@ export class AuthController {
     // auth
   }
 
+  @Redirect("/", HttpStatus.FOUND)
   @Get("google/callback")
+  @SetCookies()
   @UseGuards(AuthGuard("google"))
-  public async googleLoginCallback(@Req() req: Request): Promise<any> {
-    return this.authService.login(req.user);
+  public async googleLoginCallback(@Req() req: MyRequest): Promise<any> {
+    const jwt = await this.authService.login(req.user);
+    req._cookies = [
+      {
+        name: "jwt",
+        value: JSON.stringify(jwt),
+        options: {
+          httpOnly: true,
+        },
+      },
+    ];
   }
 
+  @Redirect("/", HttpStatus.FOUND)
   @Post("local")
+  @SetCookies()
   @UseGuards(AuthGuard("local"))
-  public async signIn(@Req() req: Request): Promise<any> {
-    return this.authService.login(req.user);
+  public async signIn(@Req() req: MyRequest): Promise<any> {
+    const jwt = await this.authService.login(req.user);
+    req._cookies = [
+      {
+        name: "jwt",
+        value: JSON.stringify(jwt),
+        options: {
+          httpOnly: true,
+        },
+      },
+    ];
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import CookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.use(CookieParser());
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>("PORT", 3000));
 }
