74c7257bf6b40fbd960f8a7668e0e23dee37bbd4
omjadas <omja.das@gmail.com>
2020-01-29 18:48:05 +1100

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import CookieParser from "cookie-parser";
+import csurf from "csurf";
+import helmet from "helmet";
 import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.use(CookieParser());
+  app.use(helmet());
+  app.use(csurf());
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>("PORT", 3000));
 }
