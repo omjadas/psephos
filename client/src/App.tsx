cb2a991be631d91aef30e1401649cfc5cff243ea
omjadas <omja.das@gmail.com>
2020-02-02 16:02:24 +1100

    convert App and Header to functional components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
 import { Header } from "./header/header";
 
-export class App extends Component {
-  public render(): JSX.Element {
-    return (
-      <div className="App">
-        <Header />
-        <BrowserRouter>
-          <Switch>
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+export const App = (_props: any): JSX.Element => {
+  return (
+    <div className="App">
+      <Header />
+      <BrowserRouter>
+        <Switch>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
--- client/src/header/header.tsx
+++ client/src/header/header.tsx
@@ -1,35 +1,17 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Form, Navbar } from "react-bootstrap";
 import { Auth } from "../auth/auth";
 
-interface State {
-  modalShow: boolean,
-}
+export const Header = (_props: any): JSX.Element => {
+  const [modalShow, setModalShow] = useState(false);
 
-export class Header extends Component<any, State> {
-  public constructor(props: any) {
-    super(props);
-
-    this.state = {
-      modalShow: false,
-    };
-
-    this.setModalShow.bind(this);
-  }
-
-  private setModalShow(show: boolean): void {
-    this.setState({ modalShow: show });
-  }
-
-  public render(): JSX.Element {
-    return (
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand className="mr-auto" href="/">Psephos</Navbar.Brand>
-        <Form inline>
-          <Button variant="outline-info" onClick={() => this.setModalShow(true)}>Sign In / Register</Button>
-        </Form>
-        <Auth show={this.state.modalShow} onHide={() => this.setModalShow(false)}/>
-      </Navbar>
-    );
-  }
-}
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand className="mr-auto" href="/">Psephos</Navbar.Brand>
+      <Form inline>
+        <Button variant="outline-info" onClick={() => setModalShow(true)}>Sign In / Register</Button>
+      </Form>
+      <Auth show={modalShow} onHide={() => setModalShow(false)} />
+    </Navbar>
+  );
+};
