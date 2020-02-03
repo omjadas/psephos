906b064c43207fdbdb29d65f410025afc6ad3eb5
omjadas <omja.das@gmail.com>
2020-02-03 19:08:27 +1100

    attach proper user for jwt auth

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -4,11 +4,10 @@ import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy } from "passport-google-oauth20";
 import { User } from "../../user/user.entity";
 import { UserService } from "../../user/user.service";
-import { AuthService } from "../auth.service";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
-  public constructor(private readonly configService: ConfigService, private readonly authService: AuthService, private readonly userService: UserService) {
+  public constructor(private readonly configService: ConfigService, private readonly userService: UserService) {
     super({
       clientID: configService.get<string>("GOOGLE_CLIENT_ID"),
       clientSecret: configService.get<string>("GOOGLE_CLIENT_SECRET"),
--- src/interfaces/Request.ts
+++ src/interfaces/Request.ts
@@ -1,6 +1,8 @@
 import { CookieSettings } from "@nestjsplus/cookies";
 import { Request as ExpressRequest } from "express";
+import { User } from "src/user/user.entity";
 
 export interface Request extends ExpressRequest {
   _cookies: CookieSettings[],
+  user: User,
 }
