f4b4c603ad7f1f3620d587634126be3cfb27d750
omjadas <omja.das@gmail.com>
2020-03-17 16:09:49 +1100

    misc fixes

--- src/interfaces/Request.ts
+++ src/interfaces/Request.ts
@@ -1,6 +1,6 @@
 import { CookieSettings } from "@nestjsplus/cookies";
 import { Request as ExpressRequest } from "express";
-import { User } from "src/user/user.entity";
+import { User } from "../user/user.entity";
 
 export interface Request extends ExpressRequest {
   _cookies: CookieSettings[],
