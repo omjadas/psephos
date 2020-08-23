90ba0ae9babfa79675bc0e482a6fd6a3d6f3842a
omjadas <omja.das@gmail.com>
2020-08-23 22:17:47 +1000

    test: fix tests for UserResolver

--- src/user/user.resolver.spec.ts
+++ src/user/user.resolver.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { Election } from "../election/election.entity";
+import { ElectionService } from "../election/election.service";
 import { User } from "./user.entity";
 import { UserResolver } from "./user.resolver";
 import { UserService } from "./user.service";
@@ -13,8 +15,13 @@ describe("UserResolver", () => {
       providers: [
         UserResolver,
         UserService,
+        ElectionService,
         {
           provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Election),
           useClass: Repository,
         },
       ],
