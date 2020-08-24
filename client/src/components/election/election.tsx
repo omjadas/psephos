ebb7ab4f1aae153603999ed8bc628f3d1acc8a4f
omjadas <omja.das@gmail.com>
2020-08-24 20:19:49 +1000

    feat(ui): use jumbotron to display election

--- client/src/components/election/election.tsx
+++ client/src/components/election/election.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
+import { Container, Jumbotron } from "react-bootstrap";
 import { useParams } from "react-router";
 import { GetElectionQuery } from "../../queries/GetElection";
 import { GetElection, GetElectionVariables } from "../../queries/types/GetElection";
@@ -24,5 +25,13 @@ export const Election = (): JSX.Element => {
     return <></>;
   }
 
-  return <>{data?.election.name}</>;
+  return (
+    <Container className="mt-3">
+      <Jumbotron>
+        <h1>{data?.election.name}</h1>
+        <p className="text-muted">Created by {data?.election.creator.name}</p>
+        <p>{data?.election.description}</p>
+      </Jumbotron>
+    </Container>
+  );
 };
