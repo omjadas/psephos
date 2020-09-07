79caaf3771ac1af4d1370a96a281b68c8046f45d
omjadas <omja.das@gmail.com>
2020-09-07 14:07:38 +1000

    test: fix tests

--- src/candidate/candidate.service.spec.ts
+++ src/candidate/candidate.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { Election } from "../election/election.entity";
+import { ElectionService } from "../election/election.service";
 import { Candidate } from "./candidate.entity";
 import { CandidateService } from "./candidate.service";
 
@@ -11,8 +13,13 @@ describe("CandidateService", () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CandidateService,
+        ElectionService,
         {
           provide: getRepositoryToken(Candidate),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Election),
           useClass: Repository,
         },
       ],
